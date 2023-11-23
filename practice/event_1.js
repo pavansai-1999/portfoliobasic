052e93c5b0e8d4c304c255a9b4fed46e6e0fac6e
pavansai-1999 <pavansai3012@gmail.com>
2023-11-23 23:30:39 +0530

    my first comment

---
+++ practice/event_1.js
@@ -0,0 +1,10 @@
+<html>
+<p>hello</p>
+<button type="button" onclick="myfun">click here for youtube</button>
+<script>
+function myfun(){
+	window.location("https://youtube.com/watch?v=PODgPW-hbfU");
+
+}
+</script>
+</html>
